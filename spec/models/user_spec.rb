85dd0e6e3b0ffbad48a6e2b4a49ec7769fa69e0e
seiya1011 <seiya.f10111@icloud.com>
2021-06-27 21:59:06 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,59 @@
 require 'rails_helper'
- RSpec.describe User, type: :model do
-   describe "ユーザー新規登録" do
-     it "nicknameが空だと登録できない" do
-       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-       user.valid?
-       expect(user.errors.full_messages).to include("Nickname can't be blank")
-     end
-     it "emailが空では登録できない" do
-       user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
-       user.valid?
-       expect(user.errors.full_messages).to include("Email can't be blank")
-     end
-   end
- end
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'first_nameが空では登録できないこと' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First_name can't be blank"
+    end
+    it 'family_nameが空では登録できないこと' do
+      @user.family_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Family_name can't be blank"
+    end
+    it 'first_name_kanaが空では登録できないこと' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First_name_kana can't be blank"
+    end
+    it 'falimy_name_kanaが空では登録できないこと' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Family_name_kana can't be blank"
+    end
+    it 'first_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+      @user.first_name = 'Taro'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First_name can't be blank"
+    end
+    it 'family_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+      @user.first_name = 'Abe'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Family_name can't be blank"
+    end
+    it 'first_name_kanaは全角カタカナ以外では登録できないこと' do
+      @user.first_name = 'Taro'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First_name_kana can't be blank"
+    end
+    it 'family_name_kanaは全角カタカナ以外では登録できないこと' do
+      @user.first_name = 'Abe'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Family_name_kana can't be blank"
+    end
+  end
+end
