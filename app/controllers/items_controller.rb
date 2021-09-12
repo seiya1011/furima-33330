513a0a514212a2b2773f550a222e5def22b92525
seiya1011 <seiya.f10111@icloud.com>
2021-09-12 17:35:04 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create]
+  before_action :move_to_index, except: [:index, :show]
   def new
     @item = Item.new
   end
@@ -28,6 +29,7 @@ class ItemsController < ApplicationController
   
     def edit
       @item =Item.find(params[:id])
+      redirect_to root_path unless current_user.id == @item.user_id
     end
 
     def update
@@ -43,5 +45,10 @@ private
 
   def items_params
     params.require(:item).permit(:product_name, :image,:info,:category_id,:product_condition_id,:shipping_charge_id,:prefecture_id,:day_to_ship_id,:price).merge(user_id: current_user.id)
+  end
+
+
+def move_to_index
+  redirect_to new_user_session_path unless user_signed_in?
   end
 end
