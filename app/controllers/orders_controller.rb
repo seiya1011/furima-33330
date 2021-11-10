32b0e2cdd062607f14fc6d8dfc6d18922b7bd297
seiya1011 <78420096+seiya1011@users.noreply.github.com>
2021-11-10 18:53:30 +0900

    comito

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
   def index
     @oders = Orders.new
-    @item = Item.new
+    @item = Item.find(params[:item_id])
    end
  
    def create
      binding.pry
    end
-end
+  end
+
