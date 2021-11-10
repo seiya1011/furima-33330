366d1b536b2e36f7b0bf9b3531a19cc5f1466d60
seiya1011 <78420096+seiya1011@users.noreply.github.com>
2021-11-10 19:08:41 +0900

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @oders = Orders.new
     @item = Item.find(params[:item_id])
